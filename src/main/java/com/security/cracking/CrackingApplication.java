7b51d685c4e71d48d41a86e62c719c0287a6aebb
PurpleZL <vladutorza64@gmail.com>
2026-01-27 16:23:30 +0100

    BASE

---
+++ src/main/java/com/security/cracking/CrackingApplication.java
@@ -0,0 +1,13 @@
+package com.security.cracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/cracking/CrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.cracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
